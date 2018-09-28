6e1270a3d96dd1cab7c3022298ae787b707ae4ab
Jeffrey Horn <jeff@jrhorn.me>
2018-09-27 17:38:40 -0700

    Make neovim and vim seamless

--- install.sh
+++ install.sh
@@ -1,1 +1,8 @@
 curl -L https://bit.ly/janus-bootstrap | bash
+
+brew install neovim
+
+# https://robots.thoughtbot.com/my-life-with-neovim
+mkdir -p ${XDG_CONFIG_HOME:=$HOME/.config}
+ln -s ~/.vim $XDG_CONFIG_HOME/nvim
+ln -s ~/.vimrc $XDG_CONFIG_HOME/nvim/init.vim
