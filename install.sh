e61589695f8b25011d13f8e5787ecaa7f844ab3c
Jeffrey Horn <jeff@jrhorn.me>
2018-09-27 17:57:04 -0700

    Reverse symbolic links between vim and neovim

--- install.sh
+++ install.sh
@@ -1,8 +1,6 @@
-curl -L https://bit.ly/janus-bootstrap | bash
-
 brew install neovim
 
 # https://robots.thoughtbot.com/my-life-with-neovim
 mkdir -p ${XDG_CONFIG_HOME:=$HOME/.config}
-ln -s ~/.vim $XDG_CONFIG_HOME/nvim
-ln -s ~/.vimrc $XDG_CONFIG_HOME/nvim/init.vim
+ln -s $XDG_CONFIG_HOME/nvim ~/.vim 
+ln -s $XDG_CONFIG_HOME/nvim/init.vim ~/.vimrc 
