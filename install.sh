a6eae0e87a5d6e1b270f50c081efd3a32f50f16e
Jeffrey Horn <jeff@jrhorn.me>
2018-10-02 11:00:40 -0600

    Install bundles

--- install.sh
+++ install.sh
@@ -8,3 +8,5 @@ ln -s $HOME/.vimrc $XDG_CONFIG_HOME/nvim/init.vim
 
 curl -fLo $HOME/vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+vim +BundleInstall! +BundleClean +q
