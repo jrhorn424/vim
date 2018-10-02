af71d0cc90969aef276eb70819892ffc8d7705c9
Jeffrey Horn <jeff@jrhorn.me>
2018-10-02 10:55:50 -0600

    Update bootstrap script

--- install.sh
+++ install.sh
@@ -1,6 +1,10 @@
+homeshick link vim
 brew install neovim
 
 # https://robots.thoughtbot.com/my-life-with-neovim
 mkdir -p ${XDG_CONFIG_HOME:=$HOME/.config}
-ln -s $XDG_CONFIG_HOME/nvim ~/.vim 
-ln -s $XDG_CONFIG_HOME/nvim/init.vim ~/.vimrc 
+ln -s $HOME/.vim $XDG_CONFIG_HOME/nvim
+ln -s $HOME/.vimrc $XDG_CONFIG_HOME/nvim/init.vim
+
+curl -fLo $HOME/vim/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
